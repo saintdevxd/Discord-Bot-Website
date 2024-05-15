a732395671d9a01dc02b4d111321efd6b49b21c6
Saint <94261435+saintdevxd@users.noreply.github.com>
2024-05-15 19:57:28 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,11 +14,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="" />
+          <meta name="twitter:creator" content="" />
+          <meta property="og:url" content="https://raven.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Raven" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -33,7 +33,7 @@ class MyDocument extends Document {
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Raven"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
