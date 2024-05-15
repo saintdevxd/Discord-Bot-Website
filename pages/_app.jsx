2d4f039c8c288093ec6a0ebd31c338eb2b05549a
Saint <94261435+saintdevxd@users.noreply.github.com>
2024-05-15 20:32:59 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -44,7 +44,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/oauth2/authorize?client_id=1235176997794353204&permissions=8&scope=bot+applications.commands",
     },
        {
       link: true,
