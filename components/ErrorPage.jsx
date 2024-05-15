067ce1e4c0a814073a5d9a9dfbb4349f13659623
Saint <94261435+saintdevxd@users.noreply.github.com>
2024-05-15 20:09:56 +0300

    Update ErrorPage.jsx

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = ({ code, message }) => {
     
     return <>
         <Head>
-            <title>{code} | Exe</title>
+            <title>{code} | Raven</title>
         </Head>
 
         <div className="max-w-7xl text-center py-20 mx-auto">
