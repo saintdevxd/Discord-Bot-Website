c5fe3017f86260c3dd63f6accb762da64cbbd972
Saint <94261435+saintdevxd@users.noreply.github.com>
2024-05-17 17:51:34 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -29,24 +29,6 @@ export default function Partners() {
                             </div>
                         </div>
                     </a>
-                    </div>
-
-
-                            <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me/partners/64db583a4292ca1066ad0a1b">
-                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1172657853056684102/1172671829673005146/maskot_profil_alev.png" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Nodesty VDS</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                   DISCOUNT CODE: CODESHARE Experience uninterrupted internet speed up to 10Gb and cheap intel i9, amd ryzen series vds with Nodesty. Shop with Code Share assurance.
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                    </a>
-                    </div>
 
                 </div>
                 <div className="py-10"></div>
